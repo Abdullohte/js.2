683e594ddd8b6f4b93c3a17c2dd3a24a7c3999fc
Abdullohte <abdulloht210@gmail.com>
2025-01-07 21:50:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+let num1 = Math.floor(Math.random() * 100) + 1; 
+let num2 = Math.floor(Math.random() * 100) + 1; 
+let product = num1 * num2;
+let dividedResult = product / 2;
+
+alert(`Числа: ${num1} и ${num2}`);
+alert(`Произведение: ${product}`);
+alert(`Результат деления в двое :${dividedResult}`);
+
+
